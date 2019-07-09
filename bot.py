8d74ebf833a287f172f39746d1107f2c1081bcb1
Alexander <alexmal74@gmail.com>
2019-07-09 08:46:56 +0500

    Code from bot.py was moved to api_requests.py

--- bot.py
+++ bot.py
@@ -1,23 +1,2 @@
-import requests
-
-
-URL = 'http://api.oboobs.ru/'
-BOOBS_COUNT = 'boobs/count'
-NOISE_COUNT = 'noise/count'
-
-def get_response(request):
-    r = requests.get(URL + request)
-    print("Status code: ", r.status_code)
-    return r
-
-
-def get_boobs_dict(response):
-    response_dict = response.json()
-    return response_dict[0]
-
-
-if __name__ == "__main__":
-    bc = get_response('boobs/')
-    print(get_boobs_dict(bc))
 
 
