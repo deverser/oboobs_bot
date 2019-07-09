61844411b65bbea9ab355d4f430ad944852e04b8
deverser <alexmal74@gmail.com>
2019-07-10 01:41:59 +0500

    getting url of last boobs photo

--- api_requests.py
+++ api_requests.py
@@ -4,18 +4,27 @@ import requests
 URL = 'http://api.oboobs.ru/'
 BOOBS_COUNT = 'boobs/count'
 NOISE_COUNT = 'noise/count'
+MEDIA = 'media.oboobs.ru/'
 
 def get_response(request):
     r = requests.get(URL + request)
-    print("Status code: ", r.status_code)
+    # print("Status code: ", r.status_code)
     return r
 
 
 def get_boobs_dict(response):
     response_dict = response.json()
     return response_dict[0]
+    
+
+def get_boobs_url():
+    """Получает URL-адрес самого свежего фото сисек с oboobs.ru"""
+    boobs = get_response('boobs/')
+    boobs_data = get_boobs_dict(boobs)
+    return MEDIA + boobs_data['preview']
 
 
 if __name__ == "__main__":
     bc = get_response('boobs/')
-    print(get_boobs_dict(bc))
+    boobs_data = get_boobs_dict(bc)
+    print(get_boobs_url())
