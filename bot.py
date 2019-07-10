393ee6ebae36375030a1ec45f4b4fc58e5f403ad
Alexander <alexmal74@gmail.com>
2019-07-10 16:08:38 +0500

    Added custom keyboard for answers of user age check

--- bot.py
+++ bot.py
@@ -11,11 +11,15 @@ import api_requests as ar
 экземпляра бота в строке ниже
 """ 
 bot = tb.TeleBot(config.token)
+keyboard = types.ReplyKeyboardMarkup(True, True, row_width=1)
+btn_y = types.KeyboardButton('Да, мне уже есть 18!')
+btn_n = types.KeyboardButton('Нет, я ещё слишком молод...')
+keyboard.row(btn_y, btn_n)
 
 @bot.message_handler(commands=["start"])
 def user_age_check(message):
     """Проверка пользователя на совершеннолетие"""
-    bot.send_message(message.chat.id, text='Вам уже исполнилось 18 лет?')
+    bot.send_message(message.chat.id, text='Вам уже исполнилось 18 лет?', reply_markup=keyboard)
 
 
 @bot.message_handler(commands=["boobs"])
